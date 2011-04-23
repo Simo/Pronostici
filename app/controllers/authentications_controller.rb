76c6249a0043f62d4ab33a0c62b751fb18023f18
Simone Bierti <sansone@MacMini.local>
2011-04-23 22:48:12 +0200

    integrata oauth bugfix2

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -5,15 +5,15 @@ class AuthenticationsController < ApplicationController
 
   def create
     auth = request.env["rack.auth"]
-    current_user.authentications.create(:provider => auth ['provider'], :uid => auth['uid'])  
-    flash[:notice] = "Authentication successful."  
+    current_user.authentications.find_or_create_by_provider_and_uid(auth['provider'], auth['uid'])
+    flash[:notice] = "Authentication successful."
     redirect_to authentications_url
   end
 
   def destroy
-    @authentication = current_user.authentications.find(params[:id])  
-    @authentication.destroy  
-    flash[:notice] = "Successfully destroyed authentication."  
+    @authentication = current_user.authentications.find(params[:id])
+    @authentication.destroy
+    flash[:notice] = "Successfully destroyed authentication."
     redirect_to authentications_url
   end
 end
